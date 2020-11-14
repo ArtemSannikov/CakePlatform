0114fe43802f226c3f56db907d963f68c1d53eb2
Artem <develop@artemsannikov.ru>
2020-11-14 18:33:17 +0600

    update

--- js/calc.js
+++ js/calc.js
@@ -1,36 +1,71 @@
-$(function(){
+$(document).ready(function(){
 
-	// Установка переменных
+	// Получаем параметры с формы
+	function getValue() {
 
-	let selectLevel; // Количество уровней
-	let selectFilling; // Начинка
-	let selectDesign; // Оформление
-	let costCake; // Общая стоиомость
+		// Количество уровней
 
-	levelCake = {
-		"1 level": 1,
-		"2 level": 2,
-		"3 level": 3,
+		let levelValue = parseInt($('input[name=level]:checked' ,'form#calcForm').val());
+		let levelText = $('input[name=level]:checked + label' ,'form#calcForm').text();
+		$('.resultCake > p span.showLevel').html(levelText);
+
+		// Начинка
+
+		let fillingValue = parseInt($('select[name=filling]' ,'form#calcForm').val());
+		let fillingText = $('select[name=filling] option:selected' ,'form#calcForm').text();
+		$('.resultCake > p span.showFilling').html(fillingText);
+
+		// Дополнительные ингредиенты
+
+		// let ingredientsValue = parseInt($('input[name=ingredients]:checked' ,'form#calcForm').val());
+		// let ingredientsText = $('input[name=ingredients]:checked + label' ,'form#calcForm').text();
+		// $('.resultCake > p span.showIngredients').html(ingredientsText);
+
+		let ingredientsValue = 0;
+
+		$('input[name=ingredients]:checkbox:checked').each(function(){
+			// alert($(this).val());
+			ingredientsValue += parseInt($(this).val());
+			let ingredientsText = $('input[name=ingredients]:checked + label' ,'form#calcForm').text();
+			console.log(ingredientsValue);
+			$('.resultCake > p span.showIngredients').html(ingredientsValue);
+
+		});
+
+		// Покрытие
+
+		let coverValue = parseInt($('select[name=cover]' ,'form#calcForm').val());
+		let coverText = $('select[name=cover] option:selected' ,'form#calcForm').text();
+		$('.resultCake > p span.showCover').html(coverText);
+
+		// Дизайн
+
+		let designValue = parseInt($('select[name=design]' ,'form#calcForm').val());
+		let designText = $('select[name=design] option:selected' ,'form#calcForm').text();
+		$('.resultCake > p span.showDesign').text(designText);
+
+
+		// Расчёт стоимости
+		let costCake = levelValue + fillingValue + ingredientsValue + coverValue + designValue;
+		console.log(costCake);
+		$('.resultCake > p span.showCost').html(costCake);
+
 	}
 
-	// Начинки
-	listFilling = {
-		"Тестовый пункт 1": 100,
-		"Тестовый пункт 2": 200,
-		"Тестовый пункт 3": 300,
-		"Тестовый пункт 4": 400,
-		"Тестовый пункт 5": 500,
-		"Тестовый пункт 6": 600,
-	};
+	getValue();
 
-	// Вставка данных в поле
-	function insertFillingCake(){
-		let html = '';
-		for(let filling in listFilling) {
-			console.log(filling);
-		}
-	}
 
-	insertFillingCake();
+	// Функция для подсчёта стоимости
+	// function calculateCake() {
 
+	// }
+
+	// calculateCake();
+
+
+	// Выполняем пересчёт стоиомости, если внесли изменения в конфигурацию
+	$('form#calcForm').on('change', function(){
+		getValue();
+		// calculateCake();
+	})
 });
